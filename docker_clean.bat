c19cce739f8c3f22c42a15f6a2dcda57d54d94b4
ivan <vanzay84@gmail.com>
2025-04-11 19:29:29 +0500

    fixed docker_clean scripts

--- docker_clean.bat
+++ docker_clean.bat
@@ -1,2 +1,9 @@
+rem Remove stopped containers
 docker container prune
+rem  Remove unused networks
+docker network prune
+rem Remove unused images
 docker image prune -a
+
+rem Remove stopped containers + unused networks + unused images + build cache
+rem docker system prune -a
--- docker_clean.sh
+++ docker_clean.sh
@@ -1,2 +1,9 @@
+# Remove stopped containers
 docker container prune
+#  Remove unused networks
+docker network prune
+# Remove unused images
 docker image prune -a
+
+# Remove stopped containers + unused networks + unused images + build cache
+# docker system prune -a
